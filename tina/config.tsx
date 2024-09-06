d056094ceeb833c29bb3bbf5b39000739a0343dd
Etay Zaslavsky <105246259+EtayZaslavsky@users.noreply.github.com>
2024-09-06 23:23:42 +0300

    copied all media to make tina cloud media work

--- tina/config.tsx
+++ tina/config.tsx
@@ -11,18 +11,18 @@ const config = defineConfig({
     process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_REF! || // Vercel branch env
     process.env.HEAD!, // Netlify branch env
   token: process.env.TINA_TOKEN!,
-  // media: {
-  //   // If you wanted cloudinary do this
-  //   // loadCustomStore: async () => {
-  //   //   const pack = await import("next-tinacms-cloudinary");
-  //   //   return pack.TinaCloudCloudinaryMediaStore;
-  //   // },
-  //   // this is the config for the tina cloud media store
-  //   tina: {
-  //     publicFolder: "public",
-  //     mediaRoot: "uploads",
-  //   },
-  // },
+  media: {
+    // If you wanted cloudinary do this
+    // loadCustomStore: async () => {
+    //   const pack = await import("next-tinacms-cloudinary");
+    //   return pack.TinaCloudCloudinaryMediaStore;
+    // },
+    // this is the config for the tina cloud media store
+    tina: {
+      publicFolder: "public",
+      mediaRoot: "uploads",
+    },
+  },
   build: {
     publicFolder: "public", // The public asset folder for your framework
     outputFolder: "admin", // within the public folder
