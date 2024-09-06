bf55dfd14fc52b7e8b0715862f5b42f40e5a22e8
Etay Zaslavsky <105246259+EtayZaslavsky@users.noreply.github.com>
2024-09-06 22:50:25 +0300

    try play with media location

--- next.config.js
+++ next.config.js
@@ -29,19 +29,4 @@ module.exports = {
       },
     ];
   },
-  async redirects() {
-    // Only apply redirects in production
-    // if (process.env.NODE_ENV === "production") {
-    return [
-      {
-        source: "/a5107ffd-adb0-45bd-8212-0bf50351d198/:image*", // Match paths like /UUID/IMAGE-FILE.JPG
-        destination: "/a5107ffd-adb0-45bd-8212-0bf50351d198/public/uploads/:image*", // Redirect to /UUID/public/uploads/IMAGE-FILE.JPG
-        permanent: false, // Use a 302 temporary redirect
-      },
-    ];
-    // }
-
-    // No redirects when not in production (e.g., localhost or development environment)
-    return [];
-  },
 };
