9364e9d2ea295b17b189b6887fed078e5a7ae57a
Etay Zaslavsky <105246259+EtayZaslavsky@users.noreply.github.com>
2024-09-24 16:45:19 +0300

    minor changes to first showdown

--- components/hero-cover.tsx
+++ components/hero-cover.tsx
@@ -1,6 +1,5 @@
 "use client";
-import React from "react";
-import { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 export const Cover = ({
     image,
@@ -10,7 +9,6 @@ export const Cover = ({
     image: string;
     title?: string;
     description?: string;
-    isLoading?: boolean;
 }) => {
     const [isLoading, setIsLoading] = useState(true); // Internal loading state
 
@@ -18,29 +16,23 @@ export const Cover = ({
         setIsLoading(false); // Stop loading once the image is loaded
     };
 
+    useEffect(() => {
+        // Preload the image to trigger the handleImageLoad correctly
+        const img = new Image();
+        img.src = image;
+        img.onload = handleImageLoad;
+    }, [image]);
+
     return (
         <div className="relative">
             {isLoading ? (
-                <div className="animate-pulse bg-gray-300 w-full pb-[68%] flex items-center justify-center">
-                    <img className="w-full" src={image} alt="Background" onLoad={handleImageLoad} />
-
+                // Loading skeleton while the image is loading
+                <div className="animate-pulse bg-gray-500 w-full pb-[68%] flex items-center justify-center">
                     {/* Skeleton Placeholder */}
-                    <div className="text-center">
-                        <div className="absolute inset-0 flex flex-col items-center justify-center text-center p-4">
-                            <div className="bg-black bg-opacity-50 px-4 py-2 rounded">
-                                <h1 className="text-5xl font-extrabold tracking-normal leading-tight title-font text-white mb-4">
-                                    {title}
-                                </h1>
-                                <p className="text-white text-lg">
-                                    {description}
-                                </p>
-                            </div>
-                        </div>
-                    </div>
                 </div>
             ) : (
                 <>
-                    <img className="w-full" src={image} alt="Background" onLoad={handleImageLoad} />
+                    <img className="w-full" src={image} alt="Background" />
                     <div className="absolute inset-0 flex flex-col items-center justify-center text-center p-4">
                         <div className="bg-black bg-opacity-50 px-4 py-2 rounded">
                             <h1 className="text-5xl font-extrabold tracking-normal leading-tight title-font text-white mb-4">
